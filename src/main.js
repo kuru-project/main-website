3943301dfe3f1c6860a300f2646923b5503a2629
BosEriko <bos.eriko@gmail.com>
2019-05-20 11:27:33 +0800

    Fixed the eslint errors on main.js

--- src/main.js
+++ src/main.js
@@ -3,22 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import * as firebase from "firebase/app";
-import "firebase";
+import * as firebase from 'firebase/app'
+import 'firebase'
 
 Vue.config.productionTip = false
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCz3laxK1vMpEpjJFXSx8UyzlH7mE4KBT0",
-    authDomain: "kuru-anime-network.firebaseapp.com",
-    databaseURL: "https://kuru-anime-network.firebaseio.com",
-    projectId: "kuru-anime-network",
-    storageBucket: "kuru-anime-network.appspot.com",
-    messagingSenderId: "195377790597",
-    appId: "1:195377790597:web:a9007d1707775d3f"
-};
+  apiKey: 'AIzaSyCz3laxK1vMpEpjJFXSx8UyzlH7mE4KBT0',
+  authDomain: 'kuru-anime-network.firebaseapp.com',
+  databaseURL: 'https://kuru-anime-network.firebaseio.com',
+  projectId: 'kuru-anime-network',
+  torageBucket: 'kuru-anime-network.appspot.com',
+  messagingSenderId: '195377790597',
+  appId: '1:195377790597:web:a9007d1707775d3f'
+}
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
 /* eslint-disable no-new */
 new Vue({
