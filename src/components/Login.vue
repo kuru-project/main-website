a9ef6d608a7840c65d6966adef02e8b3173bb302
BosEriko <bos.eriko@gmail.com>
2019-05-24 11:37:04 +0800

    Added pages for login and register

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Login Page'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Register.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      msg: 'Register Page'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
