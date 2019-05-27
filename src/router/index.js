094b718728365baff89d9e995ee14765be4bd772
BosEriko <bos.eriko@gmail.com>
2019-05-27 14:13:21 +0800

    Removed login and register for now

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import Login from '@/components/Login'
-import Register from '@/components/Register'
 
 Vue.use(Router)
 
@@ -12,16 +10,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/register',
-      name: 'Register',
-      component: Register
     }
   ]
 })
