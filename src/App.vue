2e381c65012dd908723a265d8217b007af89e27e
BosEriko <bos.eriko@gmail.com>
2019-05-29 16:34:55 +0800

    Added header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
+    <Header/>
     <router-view/>
+    <Footer/>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 import '@/assets/styles/main.css'
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data () {
+    return {
+      msg: 'Footer'
+    }
+  }
+}
+</script>
