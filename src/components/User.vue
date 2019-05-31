b67a9a7d870b524d1ba3db448024de1ff57b3790
BosEriko <bos.eriko@gmail.com>
2019-05-31 20:06:20 +0800

    trying mounted()

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="flex items-center">
+    <h3 class="mr-3">{{ onlineStatus }}</h3>
     <form>
       <input class="shadow appearance-none border rounded py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-3 text-sm" v-model="userEmail" placeholder="Email" />
       <input class="shadow appearance-none border rounded py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-3 text-sm" type="password" v-model="userPassword" placeholder="Password" />
@@ -10,7 +11,7 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapState, mapActions, mapMutations } from 'vuex'
 
 export default {
   name: 'User',
@@ -20,10 +21,22 @@ export default {
       userPassword: ''
     }
   },
+  mounted () {
+    console.log(this.onlineStatus)
+    this.UPDATE_ONLINE_STATUS()
+  },
+  computed: {
+    ...mapState([
+      'onlineStatus'
+    ])
+  },
   methods: {
     ...mapActions([
       'userRegister',
       'userLogin'
+    ]),
+    ...mapMutations([
+      'UPDATE_ONLINE_STATUS'
     ]),
     userRegisterFunction: function () {
       let credentials = {}
