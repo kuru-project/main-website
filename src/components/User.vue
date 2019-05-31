75a02d9d7590d8bd9dc710e0761b527122eb2463
BosEriko <bos.eriko@gmail.com>
2019-05-31 11:55:40 +0800

    Added text sm to the forms

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <form>
-      <input class="shadow appearance-none border rounded py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-3" v-model="userEmail" placeholder="Email" />
-      <input class="shadow appearance-none border rounded py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-3" type="password" v-model="userPassword" placeholder="Password" />
+      <input class="shadow appearance-none border rounded py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-3 text-sm" v-model="userEmail" placeholder="Email" />
+      <input class="shadow appearance-none border rounded py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-3 text-sm" type="password" v-model="userPassword" placeholder="Password" />
       <button class="no-underline hover:bg-grey-lighter bg-white py-1 px-3 text-sm text-kuru rounded shadow mr-3" v-on:click="userRegisterFunction" type="button">Register</button>
       <button class="no-underline hover:bg-grey-lighter bg-white py-1 px-3 text-sm text-kuru rounded shadow mr-3" v-on:click="userLoginFunction" type="button">Login</button>
     </form>
