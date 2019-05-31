5ce2fdf657018b43ad8e95dc9f34bab2e3a78ab9
BosEriko <bos.eriko@gmail.com>
2019-05-31 11:51:21 +0800

    Moved the register and login on the User component

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,9 +1,42 @@
 <template>
-  <div>User View</div>
+  <div>
+    <form>
+      <input v-model="userEmail" placeholder="Email" />
+      <input type="password" v-model="userPassword" placeholder="Password" />
+      <button v-on:click="userRegisterFunction" type="button">Register</button>
+      <button v-on:click="userLoginFunction" type="button">Login</button>
+    </form>
+  </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
-  name: 'User'
+  name: 'User',
+  data () {
+    return {
+      userEmail: '',
+      userPassword: ''
+    }
+  },
+  methods: {
+    ...mapActions([
+      'userRegister',
+      'userLogin'
+    ]),
+    userRegisterFunction: function () {
+      let credentials = {}
+      credentials.email = this.userEmail
+      credentials.password = this.userPassword
+      this.userRegister(credentials)
+    },
+    userLoginFunction: function () {
+      let credentials = {}
+      credentials.email = this.userEmail
+      credentials.password = this.userPassword
+      this.userLogin(credentials)
+    }
+  }
 }
 </script>
