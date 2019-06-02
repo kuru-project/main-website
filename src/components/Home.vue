e3b0244ed32294c8700836eaa5fed43f0654c6f5
BosEriko <bos.eriko@gmail.com>
2019-06-02 20:07:30 +0800

    Cleared some code for a bit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,60 +1,11 @@
 <template>
-  <div>
-    <h3>There are {{ countLinks }} links.</h3>
-    <form @submit.prevent="addLink">
-      <input class="link-input" type="text" placeholder="Add a link" v-model="newLink"/>
-    </form>
-    <ol>
-      <li v-for="(link, index) in links" v-bind:key="index">
-        {{ link }}
-        <button v-on:click="removeLinks(index)">Remove</button>
-      </li>
-    </ol>
-    <button v-on:click="removeAllLinks">Remove All Links</button>
-    <p>{{ msg }}</p>
+  <div class="container mx-auto my-3">
+    <h3>Under Maintenance</h3>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
-
 export default {
-  name: 'Home',
-  data () {
-    return {
-      newLink: '',
-      msg: ''
-    }
-  },
-  computed: {
-    ...mapState([
-      'links'
-    ]),
-    ...mapGetters([
-      'countLinks'
-    ])
-  },
-  methods: {
-    ...mapMutations([
-      'ADD_LINK',
-      'REMOVE_ALL'
-    ]),
-    ...mapActions([
-      'removeLink',
-      'removeAll'
-    ]),
-    addLink: function () {
-      this.ADD_LINK(this.newLink)
-      this.newLink = ''
-    },
-    removeLinks: function (link) {
-      this.removeLink(link)
-    },
-    removeAllLinks () {
-      this.removeAll().then(() => {
-        this.msg = 'They have been removed!'
-      })
-    }
-  }
+  name: 'Home'
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -9,30 +9,16 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     title: 'Kuru Anime',
-    links: ['http://discord.kuru-anime.com/', 'https://web.kuru-anime.com/', 'https://bot.kuru-anime.com/'],
     userIsOnline: false
   },
   getters: {
-    countLinks: state => state.links.length
   },
   mutations: {
-    ADD_LINK: (state, link) => {
-      state.links.push(link)
-    },
-    REMOVE_LINK: (state, link) => {
-      state.links.splice(link, 1)
-    },
-    REMOVE_ALL: (state) => {
-      state.links = []
-    },
     UPDATE_USER_IS_ONLINE: (state, bool) => {
       state.userIsOnline = bool
     }
   },
   actions: {
-    removeLink: (context, link) => {
-      context.commit('REMOVE_LINK', link)
-    },
     userRegister: (context, data) => {
       firebase.auth().createUserWithEmailAndPassword(data.email, data.password).catch(function (error) {
         console.log('Error Code:', error.code)
@@ -50,14 +36,6 @@ export default new Vuex.Store({
         console.log('Sign Out success!')
       }).catch(function () {
         console.log('Error in Sign Out')
-      })
-    },
-    removeAll ({commit}) {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          commit('REMOVE_ALL')
-          resolve()
-        }, 1500)
       })
     }
   }
