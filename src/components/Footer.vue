f7169a5c32f0ef7729924978fb6499b4bf1cb22b
BosEriko <bos.eriko@gmail.com>
2019-06-03 11:35:42 +0800

    Added the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="text-center">
-    {{ msg }}
+  <div class="text-center bg-gray-700 text-white p-5">
+    {{ title }} &copy; {{ year }}
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'Footer',
   data () {
     return {
-      msg: 'Footer'
+      year: new Date().getFullYear()
     }
+  },
+  computed: {
+    ...mapState([
+      'title'
+    ])
   }
 }
 </script>
