94cd9103a6a9e30a41f5023da99e861f93692d63
BosEriko <bos.eriko@gmail.com>
2019-06-12 12:48:50 +0800

    Added background image and background filter

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <div class="bg-red-200">
-      <div class="container mx-auto">Top Block</div>
+    <div class="background-image bg-cover bg-no-repeat bg-center">
+      <div class="background-filter">
+        <div class="container mx-auto h-screen">Top Block</div>
+      </div>
     </div>
     <div class="container mx-auto">Main Block</div>
   </div>
@@ -22,3 +24,13 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.background-image {
+  background-image: url('../assets/images/Wallpaper.jpg');
+}
+.background-filter {
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23000000' fill-opacity='0.4' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
+  background-color: rgba(0,0,0,.5);
+}
+</style>
