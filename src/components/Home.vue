efc0c4ba5ff9b7155ce6ec30319fb12874bd8896
BosEriko <bos.eriko@gmail.com>
2019-06-14 19:17:15 +0800

    Moved filter to app.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="background-image bg-cover bg-no-repeat bg-center mb-5">
-      <div class="background-filter">
+    <div class="bg-image bg-cover bg-no-repeat bg-center mb-5">
+      <div class="bg-filter bg-pattern">
         <div class="container mx-auto text-center text-white py-20">
           <h2 class="font-robot-condensed text-5xl">{{ title }}</h2>
           <p class="block w-64 mx-auto my-10 leading-normal text-base">Welcome to Kuru Anime community! A place where you can post your drawings, blogs, ideas and much more! Signing up is free and always will be.</p>
@@ -39,12 +39,8 @@ export default {
 </script>
 
 <style scoped>
-.background-image {
+.bg-image {
   background-image: url("../assets/images/Wallpaper.jpg");
-}
-.background-filter {
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23000000' fill-opacity='0.4' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
-  background-color: rgba(0,0,0,.5);
 }
 p.block {
   width: 100%;
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed top-0 left-0 w-screen h-screen bg-white z-50">
+  <div class="fixed top-0 left-0 w-screen h-screen bg-filter z-50">
    <div>This is the modal.</div>
    <button>[X]</button>
   </div>
