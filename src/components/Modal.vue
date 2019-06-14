2a5eaed1244c3e590743ae689d6c95011f3df1f3
BosEriko <bos.eriko@gmail.com>
2019-06-14 19:21:29 +0800

    Moved the box in the middle

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="fixed top-0 left-0 w-screen h-screen bg-filter z-50">
-   <div>This is the modal.</div>
-   <button>[X]</button>
+  <div class="fixed top-0 left-0 w-screen h-screen bg-filter z-50 flex flex-col">
+   <div class="rounded overflow-hidden shadow-lg bg-white my-auto mx-auto">
+     <div>This is the modal.</div>
+     <button>[X]</button>
+   </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'Modal'
 }
