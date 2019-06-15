57b0bb4676bc79af254e5265ab43a3ccf4b2c85a
BosEriko <bos.eriko@gmail.com>
2019-06-15 12:51:17 +0800

    Added click events for the modal and the background filter

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="fixed top-0 left-0 w-screen h-screen bg-filter z-50 flex flex-col">
-   <div class="rounded overflow-hidden shadow-lg bg-white my-auto mx-auto w-full max-w-xs p-4">
+  <div v-on:click="closeModal" class="fixed top-0 left-0 w-screen h-screen bg-filter z-50 flex flex-col">
+   <div v-on:click="clickModal" class="rounded overflow-hidden shadow-lg bg-white my-auto mx-auto w-full max-w-xs p-4">
      <div>This is the modal.</div>
    </div>
    <div class="fixed top-0 right-0 m-3">
@@ -13,6 +13,15 @@
 
 <script>
 export default {
-  name: 'Modal'
+  name: 'Modal',
+  methods: {
+    closeModal: function () {
+      alert('Close the modal!')
+    },
+    clickModal: function (e) {
+      e.stopPropagation()
+      alert('Modal has been clicked!')
+    }
+  }
 }
 </script>
