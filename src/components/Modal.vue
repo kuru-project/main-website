f7690ff71794255fc341f9479d68156e51d07009
BosEriko <bos.eriko@gmail.com>
2019-06-15 18:08:23 +0800

    Added modal state to the modal box

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="modalStatus" v-on:click="closeModal" class="fixed top-0 left-0 w-screen h-screen bg-filter z-50 flex flex-col">
    <div v-on:click="clickModal" class="rounded overflow-hidden shadow-lg bg-white my-auto mx-auto w-full max-w-xs p-4">
-     <div>This is the modal.</div>
+     <div>This is the {{ modalState }} modal.</div>
    </div>
    <div class="fixed top-0 right-0 m-3">
      <button class="text-white text-2xl cursor-pointer">
@@ -18,7 +18,8 @@ export default {
   name: 'Modal',
   computed: {
     ...mapState([
-      'modalStatus'
+      'modalStatus',
+      'modalState'
     ])
   },
   methods: {
