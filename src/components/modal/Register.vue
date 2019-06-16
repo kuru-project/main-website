51ea5cac6607da4cf24d1d1dd425b71072de7cfe
BosEriko <bos.eriko@gmail.com>
2019-06-16 16:56:22 +0800

    Put register and login to their own modal

--- src/components/modal/Register.vue
+++ src/components/modal/Register.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>
-    Register
+  <div v-if="userIsOnline === false">
+    <form>
+      <input type="email" v-model="userEmail" placeholder="Email" class="shadow appearance-none border rounded py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-3 text-sm" />
+      <input type="password" v-model="userPassword" placeholder="Password" class="shadow appearance-none border rounded py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-3 text-sm" />
+      <button type="button" v-on:click="userRegisterFunction" class="cursor-pointer no-underline hover:bg-grey-lighter bg-white py-1 px-3 text-sm text-kuru rounded shadow mr-3">Register</button>
+    </form>
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
+
 export default {
-  name: 'Register'
+  name: 'Regsiter',
+  data () {
+    return {
+      userEmail: '',
+      userPassword: ''
+    }
+  },
+  computed: {
+    ...mapState([
+      'userIsOnline'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'userRegister'
+    ]),
+    userRegisterFunction: function () {
+      let credentials = {}
+      credentials.email = this.userEmail
+      credentials.password = this.userPassword
+      this.userRegister(credentials)
+    }
+  }
 }
 </script>
--- src/components/part/User.vue
+++ src/components/part/User.vue
@@ -4,12 +4,7 @@
       <button class="no-underline hover:bg-grey-lighter bg-white py-1 px-3 text-sm text-kuru rounded shadow mr-3" v-on:click="userLogOutFunction" type="button">Log Out</button>
     </div>
     <div v-if="userIsOnline === false">
-      <form>
-        <input type="email" v-model="userEmail" placeholder="Email" class="shadow appearance-none border rounded py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-3 text-sm" />
-        <input type="password" v-model="userPassword" placeholder="Password" class="shadow appearance-none border rounded py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-3 text-sm" />
-        <button type="button" v-on:click="userRegisterFunction" class="cursor-pointer no-underline hover:bg-grey-lighter bg-white py-1 px-3 text-sm text-kuru rounded shadow mr-3">Register</button>
-        <button type="button" v-on:click="userLoginFunction" class="cursor-pointer no-underline hover:bg-grey-lighter bg-white py-1 px-3 text-sm text-kuru rounded shadow mr-3">Login</button>
-      </form>
+      Login and Register buttons here!
     </div>
   </div>
 </template>
@@ -19,12 +14,6 @@ import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'User',
-  data () {
-    return {
-      userEmail: '',
-      userPassword: ''
-    }
-  },
   computed: {
     ...mapState([
       'userIsOnline'
@@ -32,22 +21,8 @@ export default {
   },
   methods: {
     ...mapActions([
-      'userRegister',
-      'userLogin',
       'userLogOut'
     ]),
-    userRegisterFunction: function () {
-      let credentials = {}
-      credentials.email = this.userEmail
-      credentials.password = this.userPassword
-      this.userRegister(credentials)
-    },
-    userLoginFunction: function () {
-      let credentials = {}
-      credentials.email = this.userEmail
-      credentials.password = this.userPassword
-      this.userLogin(credentials)
-    },
     userLogOutFunction: function () {
       this.userLogOut()
     }
