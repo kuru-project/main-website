c65468b12c59614d4f49d671075a770e574efef6
BosEriko <bos.eriko@gmail.com>
2019-06-17 13:59:25 +0800

    Updated the login form as well

--- src/components/modal/Login.vue
+++ src/components/modal/Login.vue
@@ -1,9 +1,18 @@
 <template>
   <div v-if="userIsOnline === false">
     <form>
-      <input type="email" v-model="userEmail" placeholder="Email" class="shadow appearance-none border rounded py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-3 text-sm" />
-      <input type="password" v-model="userPassword" placeholder="Password" class="shadow appearance-none border rounded py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-3 text-sm" />
-      <button type="button" v-on:click="userLoginFunction" class="cursor-pointer no-underline hover:bg-grey-lighter bg-white py-1 px-3 text-sm text-kuru rounded shadow mr-3">Login</button>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email</label>
+        <input id="email" type="email" v-model="userEmail" placeholder="Email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
+        <input id="password" type="password" v-model="userPassword" placeholder="Password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+      </div>
+      <div class="flex items-center justify-between">
+        <button type="button" v-on:click="userLoginFunction" class="bg-kuru hover:bg-kuru-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Login</button>
+        <a class="inline-block align-baseline font-bold text-sm text-kuru hover:text-kuru-dark" href="#">Forgot Password?</a>
+      </div>
     </form>
   </div>
 </template>
