3b2bf3cedd3b975426e10656f572d22343702424
BosEriko <bos.eriko@gmail.com>
2019-06-17 13:54:23 +0800

    Added style to the register block

--- src/components/modal/Register.vue
+++ src/components/modal/Register.vue
@@ -1,10 +1,18 @@
 <template>
   <div v-if="userIsOnline === false">
-    <img src="../../assets/images/modal/register.png" alt="Register">
     <form>
-      <input type="email" v-model="userEmail" placeholder="Email" class="shadow appearance-none border rounded py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-3 text-sm" />
-      <input type="password" v-model="userPassword" placeholder="Password" class="shadow appearance-none border rounded py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-3 text-sm" />
-      <button type="button" v-on:click="userRegisterFunction" class="cursor-pointer no-underline hover:bg-grey-lighter bg-white py-1 px-3 text-sm text-kuru rounded shadow mr-3">Register</button>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email</label>
+        <input id="email" type="email" v-model="userEmail" placeholder="Email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
+        <input id="password" type="password" v-model="userPassword" placeholder="Password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+      </div>
+      <div class="flex items-center justify-between">
+        <button type="button" v-on:click="userRegisterFunction" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Register</button>
+        <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">Forgot Password?</a>
+      </div>
     </form>
   </div>
 </template>
