65ca8d3cac56e930b864a1bf720bb266686a0831
BosEriko <bos.eriko@gmail.com>
2019-06-18 11:20:33 +0800

    Moved the padding inside the modal parts

--- src/components/modal/@Modal.vue
+++ src/components/modal/@Modal.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="modalStatus" v-on:click="closeModal" class="fixed top-0 left-0 w-screen h-screen bg-filter z-50 flex flex-col">
-   <div v-on:click="clickModal" class="rounded overflow-hidden shadow-lg bg-white my-auto mx-auto w-full max-w-xs p-4">
+   <div v-on:click="clickModal" class="rounded overflow-hidden shadow-lg bg-white my-auto mx-auto w-full max-w-xs">
      <div v-if="modalState === 'Default'">
        Modal is empty.
      </div>
